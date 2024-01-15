85200e1f8a509d87cdca9aede213853cad56ba20
Filip Joelsson <filip.joelsson@weahead.se>
2024-01-15 10:37:18 +0100

    Add Histoire

---
+++ components/fk-button/fk-button.story.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+console.log("test");
+</script>
+
+<template>
+    <Story title="Fk Button">
+        <FkButton label="Test button" type="button" />
+    </Story>
+</template>
+
